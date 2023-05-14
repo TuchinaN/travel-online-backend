526ae2a3a483316a248f39cf732eb5877a1000f0
Tuchina Natalia <90835296+TuchinaN@users.noreply.github.com>
2023-05-14 14:26:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function start() {
             process.env.MONGODB_URI,
         )
 
-        app.listen(process.env.PORT || 3002, () => console.log(`Server started on port: ${PORT}`))
+        app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
     } catch (error) {
         console.log(error);
     }
