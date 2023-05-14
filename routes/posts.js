3dafae0cdbd31e932059a7d3cd46fb5ddbc84afd
Tuchina Natalia <90835296+TuchinaN@users.noreply.github.com>
2023-05-14 15:06:25 +0300

    Update posts.js
    Edit routes

--- routes/posts.js
+++ routes/posts.js
@@ -14,30 +14,30 @@ const router = new Router()
 
 //Create Post
 //http://localhost:3002/api/posts
-router.post('/', checkAuth, createPost)
+router.post('/api/posts', checkAuth, createPost)
 
 //Get All Posts
 //http://localhost:3002/api/posts
-router.get('/', getAll)
+router.get('/api/posts', getAll)
 
 //Get Post By Id
 //http://localhost:3002/api/posts/:id
-router.get('/:id', getById)
+router.get('/api/posts/:id', getById)
 
 //Get My Posts
 //http://localhost:3002/api/posts/user/me
-router.get('/user/me', checkAuth, getMyPosts)
+router.get('/api/posts/user/me', checkAuth, getMyPosts)
 
 //Delete Post By Id
 //http://localhost:3002/api/posts/:id
-router.delete('/:id', checkAuth, deletePost)
+router.delete('/api/posts/:id', checkAuth, deletePost)
 
 //Update Post
 //http://localhost:3002/api/posts/:id
-router.put('/:id', checkAuth, updatePost)
+router.put('/api/posts/:id', checkAuth, updatePost)
 
 //Get Post Comments
 //http://localhost:3002/api/posts/comments/:id
-router.get('/comments/:id', getPostCommets)
+router.get('/api/posts/comments/:id', getPostCommets)
 
 export default router
